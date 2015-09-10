8d0809c8e1d70c2c6511533231304682f93f56ef
Michael Wuergler <wuergler@gmail.com>
2015-09-10 23:16:08 +0100

    travis

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function(seconds){
+
+    seconds = parseInt(seconds, 10);
+
+    if ( isNaN(seconds) ){
+        throw new Error('Invalid value sent to convert-seconds');
+    }
+
+    var results = {};
+    results.hours = Math.floor(seconds/60/60);
+    results.minutes = Math.floor((seconds/60)%60);
+    results.seconds = Math.floor(seconds%60);
+
+    return results;
+
+};
